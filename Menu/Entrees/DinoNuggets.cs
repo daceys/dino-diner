b7b4b03e89db47c2f5817d5d2e0820b23f71bf12
Dacey Simpson <daceys@ksu.edu>
2019-09-11 16:57:13 -0500

    Sep 11 additions

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string> { "6 Chicken Nuggets" };
+
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 59 * 6;
+        }
+
+        public void AddNuggets()
+        {
+            this.Price = Price + 0.25;
+            this.Calories = Calories + 59;
+        }
+    }
+}
