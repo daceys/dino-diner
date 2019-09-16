df25f38f4dda8d5c59c0e64834cdb0909e7857d7
Dacey Simpson <daceys@ksu.edu>
2019-09-16 16:52:07 -0500

    fixed merge

--- MenuTest/Entrees/DinoNuggetsTest.cs
+++ MenuTest/Entrees/DinoNuggetsTest.cs
@@ -67,19 +67,11 @@ namespace MenuTest.Entrees
         {
             DinoNuggets dn = new DinoNuggets();
             dn.AddNugget();
-<<<<<<< HEAD
-            Assert.Equal(4.50, dn.Price);
-            dn.AddNugget();
-            Assert.Equal(4.75, dn.Price);
-            dn.AddNugget();
-            Assert.Equal(5.0, dn.Price);
-=======
             Assert.Equal(dn.Price, 4.50, 2);
             dn.AddNugget();
             Assert.Equal(dn.Price, 4.75, 2);
             dn.AddNugget();
             Assert.Equal(dn.Price, 5.0, 2);
->>>>>>> 7c1bb13d2713a6cbc09b2c052952c29a651e7984
         }
 
         [Fact]
