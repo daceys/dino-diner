f9424848dfd6f7f7761f05c57e2ce553697c0457
Dacey Simpson <daceys@users.campus>
2019-09-18 17:27:25 -0500

    sep 18 update

---
+++ Menu/Sides/MeteorMacAndCheese.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    public class MeteorMacAndCheese : Side
+    {
+        private Size size;
+
+        public override Size Size
+        {
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Small:
+                        Price = 0.99;
+                        Calories = 420;
+                        break;
+                    case Size.Medium:
+                        Price = 1.45;
+                        Calories = 490;
+                        break;
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 520;
+                        break;
+                }
+            }
+            get
+            {
+                return size;
+            }
+        }
+
+        public MeteorMacAndCheese()
+        {
+            Price = 0.99;
+            Calories = 352;
+            ingredients.Add("Macaroni Noodles");
+            ingredients.Add("Cheese Product");
+            ingredients.Add("Pork Sausage");
+        }
+    }
+}
