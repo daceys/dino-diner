3395f52f536db3b6a2b57d06138855cfeae4da7e
Dacey Simpson <daceys@ksu.edu>
2019-09-25 17:04:37 -0500

    updated classes and tests

--- Menu/Drinks/JurrasicJava.cs
+++ Menu/Drinks/JurrasicJava.cs
@@ -6,6 +6,51 @@ namespace DinoDiner.Menu.Drinks
 {
     public class JurrasicJava : Drink
     {
+        private Size size;
 
+        public override Size Size
+        {
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Small:
+                        Price = 0.59;
+                        Calories = 2;
+                        break;
+                    case Size.Medium:
+                        Price = 0.99;
+                        Calories = 4;
+                        break;
+                    case Size.Large:
+                        Price = 1.49;
+                        Calories = 8;
+                        break;
+                }
+            }
+            get
+            {
+                return size;
+            }
+        }
+
+        public JurrasicJava()
+        {
+            Price = 0.59;
+            Calories = 2;
+            ingredients.Add("Water");
+            ingredients.Add("Coffee");
+        }
+
+        public void AddIce()
+        {
+
+        }
+
+        public void LeaveRoomForCream()
+        {
+
+        }
     }
 }
--- Menu/Drinks/Sodasaurus.cs
+++ Menu/Drinks/Sodasaurus.cs
@@ -4,19 +4,49 @@ using System.Text;
 
 namespace DinoDiner.Menu.Drinks
 {
-    public enum SodasaurusFlavor
-    {
-        Cola,
-        Orange,
-        Vanilla,
-        Chocolate,
-        RootBeer,
-        Cherry,
-        Lime
-    }
-
     public class Sodasaurus : Drink
     {
-        
+        private SodasaurusFlavor flavor;
+        public SodasaurusFlavor Flavor
+        {
+            get { return flavor; }
+            set { flavor = value; }
+        }
+
+        public Sodasaurus()
+        {
+            Price = 1.50;
+            Calories = 112;
+        }
+
+        private Size size;
+        public Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Small:
+                        Price = 1.50;
+                        Calories = 112;
+                        break;
+                    case Size.Medium:
+                        Price = 2.00;
+                        Calories = 156;
+                        break;
+                    case Size.Large:
+                        Price = 2.50;
+                        Calories = 208;
+                        break;
+                }
+            }
+        }
+
+        public bool Ice = true;
     }
 }
