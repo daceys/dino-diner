86dc267731829cab248df9d6f3bf6bb77a2cb325
Dacey Simpson <daceys@users.campus>
2019-09-30 17:04:47 -0500

    changed namespaces and created interface

--- Menu/CretaceousCombo.cs
+++ Menu/CretaceousCombo.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using DinoDiner.Menu.Entrees;
-using DinoDiner.Menu.Drinks;
-using DinoDiner.Menu.Sides;
+using DinoDiner.Menu;
 
 namespace DinoDiner.Menu
 {
