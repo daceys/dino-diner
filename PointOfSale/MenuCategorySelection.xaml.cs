7749e78695a5fe37ef979420b5130e57bff64e78
Dacey Simpson <daceys@users.campus>
2019-10-09 17:31:13 -0500

    lots of additions

---
+++ PointOfSale/MenuCategorySelection.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for MenuCategorySelection.xaml
+    /// </summary>
+    public partial class MenuCategorySelection : Page
+    {
+        /// <summary>
+        /// Constructor for the menu category selection
+        /// </summary>
+        public MenuCategorySelection()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// What to do when the combo button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void SelectCombo(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new ComboSelection());
+        }
+
+        /// <summary>
+        /// What to do when the entree button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void SelectEntree(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new EntreeSelection());
+        }
+
+        /// <summary>
+        /// What to do when the side button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void SelectSide(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new SideSelection());
+        }
+
+        /// <summary>
+        /// What to do when the drink button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void SelectDrink(object sender, RoutedEventArgs args)
+        {
+            NavigationService.Navigate(new DrinkSelection());
+        }
+    }
+}
